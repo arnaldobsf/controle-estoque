ffc7585cc607df105b491cf7a5c7dca220dfc556
arnaldo filho <arnaldobsf@hotmail.com>
2020-03-04 19:06:45 -0300

    01

---
+++ controle-estoque/ControleEstoque.Web/Controllers/RelatorioController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        [Authorize]
+        public ActionResult Ressuprimento()
+        {
+            return View();
+        }
+    }
+}
